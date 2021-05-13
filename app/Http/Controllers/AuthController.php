bcf0be6ad9b0f5230432b99778e3c567666cb2d5
codeniakri001 <krishan.dixit@corewebconnections.com>
2021-05-13 19:55:57 +0530

    krishan version-2.05.13

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,7 +58,7 @@ class AuthController extends JoshController
 
         $validator = Validator::make($data, [
             'email' => 'required|email',
-            'mobile' => 'required|numeric|min:9'
+            'mobile' => 'required|numeric|min:9',
             'g-recaptcha-response' => 'required|recaptcha'
         ]);
 

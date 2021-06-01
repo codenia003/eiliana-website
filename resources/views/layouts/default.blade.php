66da710e03820145c4ab173c34a4f191296610ac
Abhishek Singh <abhishek29495@gmail.com>
2021-06-01 20:35:15 +0530

    version-2.06.01

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -189,12 +189,12 @@
                                       <a class="dropdown-item" href="#" style="font-size: 14px;font-weight: 500;color: #000 !important;">My Proposal &nbsp;&nbsp;&nbsp;&nbsp;&raquo;</a>
                                        <ul class="submenu1 submenu_item">
                                             <li>
-                                                <a class="dropdown-item submenu_down" href="{{ url('/client/my-contract-job') }}">Contract Job</a> 
-                                                {{-- <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>--}}
+                                                {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-contract-job') }}">Contract Job</a>  --}}
+                                                <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>
                                             </li>
                                             <li>
-                                                 <a class="dropdown-item submenu_down" href="{{ url('/client/my-project') }}">Project</a> 
-                                                 {{--<a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>--}}
+                                                 {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-project') }}">Project</a>  --}}
+                                                 <a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>
                                             </li>
                                         </ul>
                                     </li>
@@ -202,12 +202,12 @@
                                         <a class="dropdown-item" href="#" style="font-size: 14px;font-weight: 500;color: #000 !important;">My Postings &nbsp;&nbsp;&nbsp;&nbsp;&raquo;</a>
                                         <ul class="submenu1 submenu_item">
                                             <li>
-                                                <a class="dropdown-item submenu_down" href="{{ url('/client/my-requirement-job') }}">Contract Job</a> 
-                                                {{-- <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>--}}
+                                                {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-requirement-job') }}">Contract Job</a>  --}}
+                                                <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>
                                             </li>
                                             <li>
-                                                 <a class="dropdown-item submenu_down" href="{{ url('/client/my-requirement-project') }}">Project</a> 
-                                                 {{--<a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>--}}
+                                                 {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-requirement-project') }}">Project</a>  --}}
+                                                 <a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>
                                             </li>
                                         </ul>
                                    </li>
@@ -215,12 +215,12 @@
                                         <a class="dropdown-item" href="#" style="font-size: 14px;font-weight: 500;color: #000 !important;">My Delivery &nbsp;&nbsp;&nbsp;&nbsp;&raquo;</a>
                                         <ul class="submenu1 submenu_item">
                                             <li>
-                                                <a class="dropdown-item submenu_down" href="{{ url('/client/my-delivery-job') }}">Contract Job</a> 
-                                                {{-- <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>--}}
+                                                {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-delivery-job') }}">Contract Job</a>  --}}
+                                                <a class="dropdown-item submenu_down" onclick="togglePopup()">Contract Job</a>
                                             </li>
                                             <li>
-                                                 <a class="dropdown-item submenu_down" href="{{ url('/client/my-delivery-project') }}">Project</a> 
-                                                 {{--<a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>--}}
+                                                 {{-- <a class="dropdown-item submenu_down" href="{{ url('/client/my-delivery-project') }}">Project</a>  --}}
+                                                 <a class="dropdown-item submenu_down" onclick="togglePopup()">Project</a>
                                             </li>
                                         </ul>
                                    </li>
